c75aebaad1cacfd4e815442918424cd3801bb8be
Iasmin Marques <iasminimp7@gmail.com>
2021-11-03 11:00:40 -0300

    add: select * bd

---
+++ 117 - select/conexao.php
@@ -0,0 +1,22 @@
+<?php
+
+/* 
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Scripting/EmptyPHP.php to edit this template
+ */
+
+$host = "localhost"; #servidor, nome do servidor
+$user = "root";
+$password="";
+$dbname="celke";
+$port=3306; #porta utilizada do banco de dados
+
+$conn = mysqli_connect($host.":".$port, $user, $password,$dbname);
+if($conn){
+       echo "Conexão realizada com sucesso!";
+    
+}else{
+    die("Falha na conexão: ".mysqli_connect_error());
+}
+        
+?>     
---
+++ 117 - select/index.php
@@ -0,0 +1,26 @@
+<?php
+#vincular/ incluir com a conexao.php
+include_once './conexao.php';
+
+#selecionando o bd de USERS atribuindo a uma variavel
+//$query_users = "SELECT * FROM users"; #metodo nao indicado
+$query_users = "SELECT id,nome, email FROM users"; #metodo indicado
+
+
+#executar
+$result_users = mysqli_query($conn, $query_users);
+#conn =  variavel que possui a conexao com o BD
+#tambem pode ser escrita dessa forma:
+#mysqli_query($conn, "SELECT * FROM users");
+
+echo "<h1>Listar os usuários </h1>";
+
+while($row_user = mysqli_fetch_assoc($result_users)){
+    #var_dump($row_user);
+    echo "<br>";
+    extract($row_user);
+    echo "ID: ".$id."<br>";
+    echo "Nome: ".$nome."<br>";
+    echo "E-mail: ".$email."<br>";
+    echo "<hr>";    
+}
