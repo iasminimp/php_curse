774ea5021b7b22bfbad405ccc7c957d4b97f0718
Iasmin Marques <iasminimp7@gmail.com>
2022-01-07 12:16:59 -0300

    add: formulario criar novo usuario

--- admin-site-php/adm/app/adms/assets/js/custom.js
+++ admin-site-php/adm/app/adms/assets/js/custom.js
@@ -1,6 +1,7 @@
+//validação do formulário de LOGIN
 $(document).ready(function(){ //janela - pop up de notificação 
    // alert("Acessou a validação");
-   $('#send_login').on("submit", function(){
+   $('#send_login').on("submit", function(){ 
         if ($('#username').val()==""){
             $(".msg").html("<p style='color:#f00'> Erro: Preencha o campo usuário!</p>");
             return false;
@@ -12,3 +13,23 @@ $(document).ready(function(){ //janela - pop up de notificação
    });
 
 });
+
+//validação do formulário  de NOVO CADASTRO 
+$(document).ready(function(){ //janela - pop up de notificação 
+    // alert("Acessou a validação");
+    $('#login_new_user').on("submit", function(){ 
+         if ($('#name').val()==""){
+             $(".msg").html("<p style='color:#f00'> Erro: Preencha o campo Nome!</p>");
+             return false;
+         }
+         if($('#email').val()===""){
+            $(".msg").html("<p style='color:#f00'> Erro: Preencha o campo E-mail!</p>");
+            return false;
+        }
+         if($('#password').val()===""){
+             $(".msg").html("<p style='color:#f00'> Erro: Preencha o campo Senha!</p>");
+             return false;
+         }
+    });
+ 
+ });
--- admin-site-php/adm/app/adms/views/login_new_user.php
+++ admin-site-php/adm/app/adms/views/login_new_user.php
@@ -7,21 +7,24 @@
 
     //echo "Cadastrar usuário na página de login <br>";
 
+
     $msg=""; #iniciando a variavel com vazio (v=0;)
     $data = filter_input_array(INPUT_POST, FILTER_DEFAULT);
-    var_dump($data);
+    #var_dump($data);
     #verificando se o formulario esta preenchido corretamente para ir pro bd
     if(!empty($data['AddNewUser'])){
-        $password_encrypted = password_hash ($data['password'], PASSWORD_DEFAULT);
-        $query_user = "INSERT INTO adms_users (name, email, password, created)
-            VALUES ('".$data['name']."', '".$data['email']."', '$password_encrypted', NOW())";
-        mysqli_query($conn,$query_user);#executando a query
+        #var_dump($data);
+        #echo $data['id'];
+        $password_encrypted = password_hash($data['password'], PASSWORD_DEFAULT);
+        $query_user = "INSERT INTO adms_users (name, email, password, created) VALUES ('" . $data['name'] . "', '" . $data['email'] . "', '$password_encrypted', NOW())";
+        mysqli_query($conn, $query_user);#executando a query
         if(mysqli_insert_id($conn)){
-            $msg="<p style='color:green'>Castrado realizado com sucesso</p>";
+            $msg="<p style='color:green'>Cadastro realizado com sucesso</p>";
         }else{
            $msg = "<p style='color: #f00'>Erro: Cadastrado não realizado com sucesso</p>";
         }
     }
+
 ?>
 
 <h1>Novo Usuário</h1>
@@ -31,13 +34,14 @@
         unset ($msg);
     }
 ?>
-<form method="POST" action="">
+<span class="msg"> </span>
+<form id="login_new_user" method="POST" action="">
     <label>Nome</label>
-    <input type="text" name="name" id="name" placeholder="Digite o nome completo" autofocus required><br><br>
+    <input type="text" name="name" id="name" placeholder="Digite o nome completo" autofocus ><br><br>
     <label>E-mail</label>
-    <input type="email" name="email" id="email" placeholder="Digite o seu melhor e-mail" required><br><br>
+    <input type="email" name="email" id="email" placeholder="Digite o seu melhor e-mail" ><br><br>
     <label>Senha</label>
-    <input type="password" name="password" id="password" placeholder="Digite a senha" required><br><br>
+    <input type="password" name="password" id="password" placeholder="Digite a senha" ><br><br>
 
     <input type="submit" value="Cadastrar" name="AddNewUser">
 </form>
